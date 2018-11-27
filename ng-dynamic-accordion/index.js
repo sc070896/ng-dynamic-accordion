87e0db06995a80fb21fea1fad65377cb79cd299b
Santosh Choudhary <43815666+sc070896@users.noreply.github.com>
2018-11-27 15:01:37 +0530

    Delete index.js

--- ng-dynamic-accordion/index.js
+++
@@ -1,3 +0,0 @@
-export { AccordionModule } from './src/app/accordion/accordion.module';
-export { CollapseComponent } from './src/app/accordion/collapse/collapse.component';
-//# sourceMappingURL=index.js.map
